9f1ac91c14e5e8e72562da173e4354379dde8713
tanickell <tnickell1@cnm.edu>
2025-06-11 14:35:34 -0600

    Completed database class.

--- app/src/main/java/edu/cnm/deepdive/notes/service/dao/UserDao.java
+++ app/src/main/java/edu/cnm/deepdive/notes/service/dao/UserDao.java
@@ -1,11 +1,15 @@
 package edu.cnm.deepdive.notes.service.dao;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
 import androidx.room.Update;
 import edu.cnm.deepdive.notes.model.entity.User;
 import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Maybe;
 import io.reactivex.rxjava3.core.Single;
 import java.util.List;
 
@@ -14,6 +18,14 @@ public interface UserDao {
 
   @Insert
   Single<Long> insert(User user); // When should we use user.setId(...) to update the object in memory // single
+
+  default Single<User> insertAndGet(User user) {
+    return insert(user)
+        .map((id) -> {
+          user.setId(id);
+          return user;
+        });
+  }
 
   @Insert(onConflict = OnConflictStrategy.IGNORE)
   Single<List<Long>> insert(List<User> users); // item in an iterable (could be Collection or Iterable)
@@ -30,6 +42,22 @@ public interface UserDao {
   @Update(onConflict = OnConflictStrategy.IGNORE)
   Single<Integer> update(User... users);
 
+  @Delete
+  Completable delete(User user);
 
+  @Delete
+  Completable delete(List<User> users);
+
+  @Delete
+  Completable delete(User... users);
+
+  @Query("SELECT * FROM user WHERE user_id = :userId")
+  LiveData<User> select(long userId);
+
+  @Query("SELECT * FROM user WHERE oauth_key = :oauthKey")
+  Maybe<User> select(String oauthKey);
+
+  @Query("SELECT * FROM user ORDER BY display_name ASC") // ASC or DESC; if blank, ASC assumed (default)
+  LiveData<List<User>> select();
 
 }
