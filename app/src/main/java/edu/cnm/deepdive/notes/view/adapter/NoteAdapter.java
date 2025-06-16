c9be49214e09fd418b7ade9bfb151499cdb9b2f5
tanickell <tnickell1@cnm.edu>
2025-06-16 16:58:03 -0600

    Working on recyclerview for notes.

--- app/src/main/java/edu/cnm/deepdive/notes/view/adapter/NoteAdapter.java
+++ app/src/main/java/edu/cnm/deepdive/notes/view/adapter/NoteAdapter.java
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView.Adapter;
 import androidx.recyclerview.widget.RecyclerView.ViewHolder;
 import dagger.hilt.android.qualifiers.ActivityContext;
 import dagger.hilt.android.scopes.FragmentScoped;
+import edu.cnm.deepdive.notes.databinding.ItemNoteBinding;
 import edu.cnm.deepdive.notes.model.entity.Note;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
@@ -35,15 +36,17 @@ public class NoteAdapter extends Adapter<ViewHolder> {
   @NonNull
   @Override
   public ViewHolder onCreateViewHolder(@NonNull ViewGroup container, int itemType) {
-    // TODO: 6/16/25 Inflate the itemLayout and pass it to the Holder constructor, then return the
+    // DONE: 6/16/25 Inflate the itemLayout and pass it to the Holder constructor, then return the
     //  new Holder.
-    return null;
+    ItemNoteBinding binding = ItemNoteBinding.inflate(inflater, container, false);
+    return new Holder(binding, formatter);
   }
 
   @Override
   public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
-    // TODO: 6/16/25 Get the Note at the specified position; pass that note and the formatter (and
+    // DONE: 6/16/25 Get the Note at the specified position; pass that note and the formatter (and
     //  the position) to the Holder.bind method.
+    ((Holder) viewHolder).bind(position, notes.get(position));
   }
 
   @Override
@@ -60,12 +63,21 @@ public class NoteAdapter extends Adapter<ViewHolder> {
 
   private static class Holder extends ViewHolder {
 
-    Holder (@NonNull View itemView) {
-      super(itemView);
+    private final ItemNoteBinding binding;
+    private final DateTimeFormatter formatter;
+    Holder (@NonNull ItemNoteBinding binding, @NonNull DateTimeFormatter formatter) {
+      super(binding.getRoot());
+      this.binding = binding;
+      this.formatter = formatter;
     }
 
-    // TODO: 6/16/25 Define bind method to take a specified Note and insert its contents into the
+    // DONE: 6/16/25 Define bind method to take a specified Note and insert its contents into the
     //  widgets referenced by the binding object.
+
+    void bind(int position, Note note) {
+      // TODO: 6/16/25 Set contents of binding fields to hold contents of noted; attach event
+      //  listeners to binding.getRoot().
+    }
 
   }
 
